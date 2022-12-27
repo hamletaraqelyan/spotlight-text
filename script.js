0ead61356f55fc4528a8705381124437ea469dec
hamlet <hamlet.araqelyan1995@gmail.com>
2022-12-28 03:50:04 +0400

    Mobile version added!

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 $(function () {
-    const spotlightEl = $('.focus');
-    $(document).mousemove(e => {
-        const {pageX, pageY} = e;
-        spotlightEl.css('background', `radial-gradient(circle at ${pageX}px ${pageY}px, transparent, rgb(6 9 16 / 90%) 35%)`);
-    })
+    const ua = navigator.userAgent;
+    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua) || /Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+        $('.mobilePopup').addClass('show');
+    } else {
+        const spotlightEl = $('.focus');
+        $(document).mousemove(e => {
+            const {pageX, pageY} = e;
+            spotlightEl.css('background', `radial-gradient(circle at ${pageX}px ${pageY}px, transparent, rgb(6 9 16 / 90%) 35%)`);
+        })
+    }
 });
