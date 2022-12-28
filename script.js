a348fa3cd56773c0b17361a2c61f32a4b78c814f
hamlet <hamlet.araqelyan1995@gmail.com>
2022-12-28 20:13:10 +0400

    Fixes!

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(function () {
         const spotlightEl = $('.focus');
         $(document).mousemove(e => {
             const {pageX, pageY} = e;
-            spotlightEl.css('background', `radial-gradient(circle at ${pageX}px ${pageY}px, transparent, rgb(6 9 16 / 90%) 35%)`);
+            spotlightEl.css('background', `radial-gradient(circle at ${pageX}px ${pageY}px, transparent, rgb(6 9 16 / 95%) 35%)`);
         })
     }
 });
